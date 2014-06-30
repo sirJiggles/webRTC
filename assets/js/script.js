e771a86415b9b709f7157a343e90123f5301a4cb
Gareth Fuller <gareth@honestideas.com>
2014-06-30 23:34:58 +0100

    Starting to play with the remote video feed problems as to what this is ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ var webRTCApp = function(){
 	this.initEvents();
 };
 
- webRTCApp.prototype.gotStream = function(stream) {
+webRTCApp.prototype.gotLocalStream = function(stream) {
 
 	window.stream = stream; // stream available to console
 	if (window.URL) {
@@ -30,7 +30,7 @@ var webRTCApp = function(){
 
 
   	// handle the audio data from the local stream
-  	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  	/*window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var audioContext = new AudioContext();
 
     // Create an AudioNode from the stream
@@ -38,8 +38,53 @@ var webRTCApp = function(){
 
     // Connect it to destination to hear yourself
     // or any other node for processing!
-    mediaStreamSource.connect(audioContext.destination);
+    mediaStreamSource.connect(audioContext.destination);*/
+
+    // start running the remote connection test
+    this.settupConnection();
 };
+
+// this is where we start the call
+webRTCApp.prototype.settupConnection = function(){
+	var servers = null;
+
+  	this.localPeerConnection = new webkitRTCPeerConnection(servers);
+	this.localPeerConnection.onicecandidate = this.gotLocalIceCandidate;
+
+	this.remotePeerConnection = new webkitRTCPeerConnection(servers);
+  	this.remotePeerConnection.onicecandidate = this.gotRemoteIceCandidate;
+  	this.remotePeerConnection.onaddstream = this.gotRemoteStream;
+
+  	this.localPeerConnection.addStream(window.stream);
+	this.localPeerConnection.createOffer(this.gotLocalDescription);
+};
+
+webRTCApp.prototype.gotLocalDescription = function(description){
+  	this.localPeerConnection.setLocalDescription(description);
+  	this.remotePeerConnection.setRemoteDescription(description);
+  	this.remotePeerConnection.createAnswer(this.gotRemoteDescription);
+}
+
+webRTCApp.prototype.gotRemoteDescription = function(description){
+  	this.remotePeerConnection.setLocalDescription(description);
+	this.localPeerConnection.setRemoteDescription(description);
+}
+
+webRTCApp.prototype.gotRemoteStream = function(event){
+  	this.remoteFeed.src = URL.createObjectURL(event.stream);
+}
+
+webRTCApp.prototype.gotLocalIceCandidate = function(event){
+  	if (event.candidate) {
+    	this.remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+  	}
+}
+
+webRTCApp.prototype.gotRemoteIceCandidate = function(event){
+	if (event.candidate) {
+		this.localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+	}
+}
 
 // error callback for user media :D
 webRTCApp.prototype.errorCallback = function(error){
@@ -66,11 +111,12 @@ window.onload = (function () {
 	app.remoteFeed = $('#remoteFeed');
 
 	var constraints = {
-		audio:true, 
+		audio:false, 
 		video:true
 	};
 
-	navigator.getUserMedia(constraints, app.gotStream, app.errorCallback);
+	// get the local stream, when thats run we will create the fake remote connection
+	navigator.getUserMedia(constraints, app.gotLocalStream, app.errorCallback);
 
 })();
 
